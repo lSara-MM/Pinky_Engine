69c45235cbbcf9478e6888f49d291f5fd6be8139
lSara-MM <sqmartinezm@gmail.com>
2023-10-06 09:59:16 +0200

    assimp test

---
+++ Pinky_Engine/Assimp/Assimp.cpp
@@ -0,0 +1,19 @@
+#include "Assimp.h"
+
+#include "../Source/Globals.h"
+
+bool ai::LoadScene()
+{
+	//const aiScene* scene = aiImportFile(file_path, aiProcessPreset_TargetRealtime_MaxQuality);
+	//if (scene != nullptr && aiScene > HasMeshes())
+	//{
+	//	// Use scene->mNumMeshes to iterate on scene->mMeshes array
+	//	aiReleaseImport(scene);
+	//}
+	//else
+	//{
+	//	LOG("Error loading scene % s", file_path);
+	//	return false;
+	//}
+	return true;
+}
---
+++ Pinky_Engine/Assimp/Assimp.h
@@ -0,0 +1,22 @@
+#include "../Assimp/include/cimport.h"
+#include "../Assimp/include/scene.h"
+#include "../Assimp/include/postprocess.h"
+#pragma comment (lib, "Assimp/libx86/assimp.lib")
+
+#include "../Source/Globals.h"
+
+namespace ai
+{
+	struct mesh
+	{
+		uint id_index = 0; // index in VRAM
+		uint num_index = 0;
+		uint* index = nullptr;
+
+		uint id_vertex = 0; // unique vertex in VRAM
+		uint num_vertex = 0;
+		float* vertex = nullptr;
+	};
+
+	bool LoadScene();
+}
