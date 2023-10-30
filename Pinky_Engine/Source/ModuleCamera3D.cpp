b9b7bb0caa4afaa743d751967172210732b4b908
AndyCubico <astartes99@gmail.com>
2023-10-30 08:20:46 +0100

    Up/Down camera movement

--- Pinky_Engine/Source/ModuleCamera3D.cpp
+++ Pinky_Engine/Source/ModuleCamera3D.cpp
@@ -66,6 +66,8 @@ update_status ModuleCamera3D::Update(float dt)
 		if (App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)
 			speed = 20.0f * dt;
 
+		if (App->input->GetKey(SDL_SCANCODE_E) == KEY_REPEAT) newPos.y += speed;
+		if (App->input->GetKey(SDL_SCANCODE_Q) == KEY_REPEAT) newPos.y -= speed;
 		if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) newPos -= Z * speed;
 		if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) newPos += Z * speed;
 		if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) newPos -= X * speed;
