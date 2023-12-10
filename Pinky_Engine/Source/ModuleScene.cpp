5b34e97791c304ac1d4c51a28c3f5482173bd7f3
Sara-MM <sqmartinezm@gmail.com>
2023-12-10 20:22:31 +0100

    Update ModuleScene.cpp

--- Pinky_Engine/Source/ModuleScene.cpp
+++ Pinky_Engine/Source/ModuleScene.cpp
@@ -69,7 +69,7 @@ bool ModuleScene::Init()
 bool ModuleScene::Start()
 {
 	LOG("Starting scene");
-	//App->resource->ImportToScene("BakerHouse.fbx", PINKY_ASSETS_AUX "3dObject\\baker_house\\");
+	App->resource->ImportToScene("street2.fbx", PINKY_ASSETS_AUX "3dObject\\street\\");
 	//App->resource->ImportToScene("Baker_house.dds", PINKY_ASSETS_AUX "3dObject\\baker_house\\");
 	return true;
 }
